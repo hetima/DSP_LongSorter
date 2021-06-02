2e783b1bcb5c4d9d65ec94f5ebf548f526aa3b7e
hetima <hetima@hetima.com>
2021-06-03 02:43:36 +0900

    Ctrl押してたら角度緩和

--- LongSorter/LongSorter.cs
+++ LongSorter/LongSorter.cs
@@ -5,6 +5,7 @@ using System;
 using System.Reflection;
 using System.Reflection.Emit;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using UnityEngine;
 using UnityEngine.UI;
@@ -74,6 +75,71 @@ namespace LongSorter
             public static IEnumerable<CodeInstruction> BuildTool_Click_Transpiler(IEnumerable<CodeInstruction> instructions)
             {
                 return CheckBuildConditions_Transpiler(instructions);
+            }
+
+            //Ldc_R4 と OpCodes.Call の operand は同サイズ(4バイト)なので置き換え可能 メソッドの返り値がスタックに積まれる
+            [HarmonyTranspiler, HarmonyPatch(typeof(BuildTool_Inserter), "DeterminePreviews")]
+            public static IEnumerable<CodeInstruction> BuildTool_Inserter_DeterminePreviews_Transpiler(IEnumerable<CodeInstruction> instructions)
+            {
+                MethodInfo m = typeof(System.Collections.Generic.List<BuildTool_Inserter.PosePair>).GetMethod("Add");
+                MethodInfo ac11 = typeof(LongSorter.Patch).GetMethod("AngleCorrection11");
+                MethodInfo ac14 = typeof(LongSorter.Patch).GetMethod("AngleCorrection14");
+                MethodInfo ac40 = typeof(LongSorter.Patch).GetMethod("AngleCorrection40");
+                List<CodeInstruction> ins = instructions.ToList();
+
+                //BuildTool_Inserter.PosePair.Add() するちょっと前に比較してる
+                //ldc.r4 11 or 14
+                for (int i = 0; i < ins.Count; i++)
+                {
+                    if (ins[i].opcode == OpCodes.Callvirt && ins[i].operand is MethodInfo o && o == m)
+                    {
+                        for (int j = i-1; j > i - 22; j--)
+                        {
+                            if (ins[j].opcode == OpCodes.Ldc_R4 && ins[j].operand is float num1)
+                            {
+                                if (num1 == 11f)
+                                {
+                                    ins[j].opcode = OpCodes.Call;
+                                    ins[j].operand = ac11;
+                                    break;
+                                }
+                                else if (num1 == 14f)
+                                {
+                                    ins[j].opcode = OpCodes.Call;
+                                    ins[j].operand = ac14;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+
+                    //IL_0c44: ldloc.s num1_V_54
+                    //IL_0c46: ldc.r4  40
+                    if (ac40 != null && ins[i].opcode == OpCodes.Ldloc_S && i + 1 < ins.Count)
+                    {
+                        if (ins[i + 1].opcode == OpCodes.Ldc_R4 && ins[i + 1].operand is float num2 && num2 == 40f)
+                        {
+                            ins[i + 1].opcode = OpCodes.Call;
+                            ins[i + 1].operand = ac40;
+                            ac40 = null; //1回だけ
+                        }
+                    }
+                }
+                return ins.AsEnumerable();
+            }
+
+            //角度なので90とか180とか返しとけば足りそう
+            public static float AngleCorrection11()
+            {
+                return VFInput.control ? 1000f : 11f;
+            }
+            public static float AngleCorrection14()
+            {
+                return VFInput.control ? 1000f : 14f;
+            }
+            public static float AngleCorrection40()
+            {
+                return VFInput.control ? 1000f : 40f;
             }
         }
 
